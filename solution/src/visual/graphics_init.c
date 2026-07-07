f8d583a4ebe61287d972f82967a2a290b3c9311a
solsarok <solsarok@gmail.com>
2026-07-07 17:19:15 +0200

    norminette graphics_init.c

--- solution/src/visual/graphics_init.c
+++ solution/src/visual/graphics_init.c
@@ -8,15 +8,57 @@ int	load_texture(t_graphics *graphics, t_image *img, const char *path)
 		return (0);
 	}
 	img->ptr = mlx_xpm_file_to_image(graphics->mlx, (char *)path,
-		&img->width, &img->height);
+			&img->width, &img->height);
 	if (!img->ptr)
 	{
 		fprintf(stderr, "Error\nFailed to load texture %s.\n", path);
 		return (0);
 	}
 	img->data = mlx_get_data_addr(img->ptr, &img->bpp, &img->size_line,
-		&img->endian);
+			&img->endian);
 	return (1);
+}
+
+static int	graphics_preparation(t_graphics *gr)
+{
+	gr->mlx = mlx_init();
+	if (!gr->mlx)
+	{
+		printf("Error\nmlx initialization failed.\n");
+		return (0);
+	}
+	gr->window = mlx_new_window(gr->mlx,
+			WINDOW_WIDTH, WINDOW_HEIGHT, "cub3D");
+	if (!gr->window)
+	{
+		printf("Error\nWindow initialization failed.\n");
+		return (0);
+	}
+	gr->screen.ptr = mlx_new_image(gr->mlx,
+			WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (!gr->screen.ptr)
+	{
+		printf("Error\nImage initialization failed.\n");
+		return (0);
+	}
+	gr->screen.data = mlx_get_data_addr(gr->screen.ptr, &gr->screen.bpp,
+			&gr->screen.size_line, &gr->screen.endian);
+	gr->screen.width = WINDOW_WIDTH;
+	gr->screen.height = WINDOW_HEIGHT;
+	return (1);
+}
+
+static void	set_player_angle(t_graphics *graphics,
+	char player_orientation)
+{
+	if (player_orientation == 'N')
+		graphics->player.angle = 3 * PI / 2;
+	else if (player_orientation == 'S')
+		graphics->player.angle = PI / 2;
+	else if (player_orientation == 'E')
+		graphics->player.angle = 0;
+	else if (player_orientation == 'W')
+		graphics->player.angle = PI;
 }
 
 int	init_graphics(t_graphics *graphics, t_scene *scene)
@@ -25,41 +67,12 @@ int	init_graphics(t_graphics *graphics, t_scene *scene)
 
 	ft_bzero(graphics, sizeof(t_graphics));
 	ft_bzero(graphics->door_state, sizeof(graphics->door_state));
-	graphics->mlx = mlx_init();
-	if (!graphics->mlx)
-	{
-		printf("Error\nmlx initialization failed.\n");
+	if (!graphics_preparation(graphics))
 		return (0);
-	}
-	graphics->window = mlx_new_window(graphics->mlx, WINDOW_WIDTH,
-			WINDOW_HEIGHT, "cub3D");
-	if (!graphics->window)
-	{
-		printf("Error\nWindow initialization failed.\n");
-		return (0);
-	}
-	graphics->screen.ptr = mlx_new_image(graphics->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	if (!graphics->screen.ptr)
-	{
-		printf("Error\nImage initialization failed.\n");
-		return (0);
-	}
-	graphics->screen.data = mlx_get_data_addr(graphics->screen.ptr,
-		&graphics->screen.bpp, &graphics->screen.size_line,
-		&graphics->screen.endian);
-	graphics->screen.width = WINDOW_WIDTH;
-	graphics->screen.height = WINDOW_HEIGHT;
 	graphics->scene = scene;
 	graphics->player.x = scene->map.player_position_x * 64 + 32;
 	graphics->player.y = scene->map.player_position_y * 64 + 32;
-	if (scene->map.player_orientation == 'N')
-		graphics->player.angle = 3 * PI / 2;
-	else if (scene->map.player_orientation == 'S')
-		graphics->player.angle = PI / 2;
-	else if (scene->map.player_orientation == 'E')
-		graphics->player.angle = 0;
-	else if (scene->map.player_orientation == 'W')
-		graphics->player.angle = PI;
+	set_player_angle(graphics, scene->map.player_orientation);
 	graphics->move_speed = 7.0;
 	graphics->rotate_speed = 0.1;
 	i = 0;
@@ -90,10 +103,9 @@ void	free_graphics(t_graphics *graphics)
 		mlx_destroy_image(graphics->mlx, graphics->east.ptr);
 	if (graphics->window)
 		mlx_destroy_window(graphics->mlx, graphics->window);
-	#ifdef __linux__
-		mlx_destroy_display(mlx);
-	#endif
 	if (graphics->mlx)
+	{
+		//mlx_destroy_display(graphics->mlx);
 		free(graphics->mlx);
+	}
 }
-
